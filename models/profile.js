013ee7be80580d83ccb263ecd47b51c0faa5da89
Nate Morgan <nathanielrmorgan@gmail.com>
2022-10-20 20:12:19 -0400

    updates profile schema to match ERD

--- models/profile.js
+++ models/profile.js
@@ -4,7 +4,22 @@ const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
   name: String,
-  photo: String
+  shared: [{ 
+    type: Schema.Types.ObjectId, 
+    ref: 'TTReview' }],
+  wishlist: [{ 
+    type: Schema.Types.ObjectId, 
+    ref: 'TTReview' }],
+  visited: [{ 
+    type: Schema.Types.ObjectId, 
+    ref: 'Restaurant' }],
+  followers: [{ 
+    type: Schema.Types.ObjectId, 
+    ref: 'Profile' }],
+  following: [{ 
+    type: Schema.Types.ObjectId, 
+    ref: 'Profile' }],
+  tots: Number
 },{
   timestamps: true,
 })
