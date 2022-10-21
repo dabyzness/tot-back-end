1e0e8e2f318393dc7d5633fa3c4f08855dc1d1bf
Nate Morgan <nathanielrmorgan@gmail.com>
2022-10-21 15:12:11 -0400

    complete restaurants controller

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -1,1 +1,59 @@
 import { Restaurant } from '../models/restaurant.js'
+
+const create = async (req, res) => {
+  try {
+    const restaurant = await Restaurant.create(req.body)
+    res.status(201).json(restaurant)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(error)
+  }
+}
+
+const index = async (req, res) => {
+  try {
+    const restaurants = await Restaurant.find({})
+    res.status(200).json(restaurants)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const show = async (req, res) => {
+  try {
+    const restaurant = await Restaurant.findById(req.params.id)
+    res.status(200).json(restaurant)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const update = async (req, res) => {
+  try {
+    const restaurant = await Restaurant.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      { new: true }
+    )
+    res.status(200).json(restaurant)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+const deleteRestaurant = async (req, res) => {
+  try {
+    const restaurant = await Restaurant.findByIdAndDelete(req.params.id)
+    res.status(200).json(restaurant)
+  } catch (error) {
+    res.status(500).json(error)
+  }
+}
+
+export {
+  create,
+  index,
+  show,
+  update,
+  deleteRestaurant as delete
+}
